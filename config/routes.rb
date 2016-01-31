54bd9d8d082b27685800b14b18417c652f68a9e2
medwards1771 <meredith.edwards1771@gmail.com>
2016-01-31 13:57:29 -0500

    restyles view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   root to: "home#show"
   post 'tags', to: 'home#create_tags', as: 'create_tags'
   delete 'tags', to: 'home#destroy_tags', as: 'destroy_tags'
-  resources :paintings
   resources :tags
 end
